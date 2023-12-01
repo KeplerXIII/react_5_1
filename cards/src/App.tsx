a28212eefc7d7428c702956572ed09fc9a874caf
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-12-01 15:10:22 +0300

    upd types

--- cards/src/App.tsx
+++ cards/src/App.tsx
@@ -5,26 +5,25 @@ function App() {
 
   type CardProps = {
     title: string
-  }
-
-  type CardImgProps = {
+    text: string
+    children: React.ReactNode
     imgSrc: string
   }
 
-  const CardImg: React.FC<CardImgProps> = ({ imgSrc }) => {
+  const CardImg: React.FC<Pick<CardProps, 'imgSrc'>> = ({ imgSrc }) => {
     if (imgSrc) return <img src={imgSrc} className="card-img-top" alt="Кратинка"></img>
   }
 
-  const Card: React.FC<CardProps> = ({ title }) => {
+  const Card: React.FC<Omit<CardProps, 'imgSrc'>> = ({ title, text, children }) => {
     return (
       <div className="card text-light bg-dark h-auto" style={{
         maxWidth: '250px',
         minWidth: '250px'
       }}>
-        <CardImg imgSrc='http://alfa.dev-top-it.ru/local/templates/alpha/img/nophoto.jpg'/>
+        { children }
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">{text}</p>
           <a href="#" className="btn p-3 mb-2 bg-secondary text-white">Go somewhere</a>
         </div>
       </div >
@@ -33,7 +32,9 @@ function App() {
 
   return (
     <>
-      <Card title='Card title' />
+      <Card title='Test title' text='Test text'>
+        <CardImg imgSrc='http://alfa.dev-top-it.ru/local/templates/alpha/img/nophoto.jpg'></CardImg>
+      </Card>
     </>
   )
 }
